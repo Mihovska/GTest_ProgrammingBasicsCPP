9bf14d0302f9c77508a0965492d41bd9050c2571
Violeta Mihovska <vili_mihovska@live.com>
2024-04-13 16:22:56 +0300

    updating files

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,67 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <cmath>
+#include <algorithm>
+#include <array>
 #include <cctype>
+#include <cfloat>
 #include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <locale>
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 #ifdef TESTING
 #include <gtest/gtest.h>
 #endif
 
 int process(std::istream &cin, std::ostream &cout) {
-    std::string destination;
-    double ticketPrice = 0;
-    cin >> destination;
-    while (destination != "End") {
-        cin >> ticketPrice;
-        double collectedMoney = 0;
-        while (collectedMoney < ticketPrice) {
-            double earnedMoney;
-            cin >> earnedMoney;
-            collectedMoney += earnedMoney;
-        }
-        if (collectedMoney >= ticketPrice) {
-            cout << "Going to " << destination << "!" << std::endl;
-        }
-        cin >> destination;
+  std::vector<std::string> firstVector;
+  std::vector<std::string> secondVector;
+
+  std::string line;
+
+  getline(cin, line);
+  std::istringstream issFirst(line);
+  std::string num;
+  while (issFirst >> num) {
+    firstVector.push_back(num);
+  }
+
+  getline(cin, line);
+  std::istringstream issSecond(line);
+  while (issSecond >> num) {
+    secondVector.push_back(num);
+  }
+
+  std::string output;
+  int size = std::min(firstVector.size(), secondVector.size());
+
+  for (int i = 0; i < size; ++i) {
+    if (firstVector[i] < secondVector[i]) {
+      cout << ">";
+    } else if (firstVector[i] > secondVector[i]) {
+      cout << "<";
+    } else {
+      cout << "=";
     }
-    return 0;
+  }
+
+  return 0;
 }
 
 int main(int argc, char **argv) {
 #ifdef TESTING
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 #else
-    process(std::cin, std::cout);
-    return 0;
+  process(std::cin, std::cout);
+  return 0;
 #endif
 }
