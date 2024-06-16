4e058275520e826d01b0f22cd4a99ef17cdfbf12
Violeta Mihovska <vili_mihovska@live.com>
2024-06-16 12:31:34 +0300

    deep-space

--- src/main.cpp
+++ src/main.cpp
@@ -16,62 +16,77 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <map>
 
 #ifdef TESTING
 #include <gtest/gtest.h>
 #endif
 
+const char ASTEROID = '#';
+const char COMET = '$';
+const char EMPTY_SEGMENT = '.';
+const char REPLACE_SYMBOL = '+';
+const std::vector<char> STARS = {'O', 'B', 'A', 'F', 'G', 'K', 'M', 'L', 'T', 'Y'};
+
 int process(std::istream &cin, std::ostream &cout) {
-  std::stack<std::string> flowA;
-  std::queue<std::string> flowB;
+  std::vector<std::vector<char>> matrix;
+  std::string line;
 
-  std::string fishName;
-  std::string flowType;
+  while (std::getline(cin, line) && line != "end") {
+    std::vector<char> row(line.begin(), line.end());
+    matrix.push_back(row);
+  }
 
-  while (cin >> fishName && fishName != "end") {
-    cin >> flowType;
+  std::string replacements;
+  std::getline(cin, replacements);
 
-    if (flowType == "A") {
-      flowA.push(fishName);
-    } else {
-      flowB.push(fishName);
+  std::map<char, int> starCount;
+  int planetCount = 0;
+  int cometCount = 0;
+  int asteroidCount = 0;
+
+  for (const auto &row : matrix) {
+    for (auto ch : row) {
+      if (ch == ASTEROID) {
+        asteroidCount++;
+      } else if (ch == COMET) {
+        cometCount++;
+      } else if (ch >= 1 && ch <= 9) {
+        planetCount++;
+      } else {
+        auto it = std::find(STARS.begin(), STARS.end(), ch);
+        if (it != STARS.end()) {
+          size_t pos = it - STARS.begin();
+          starCount[pos]++;
+        }
+       }
     }
   }
 
-  if (flowA.empty()) {
-    cout << "Flow A:" << std::endl << "<EMPTY>" << std::endl;
-  } else {
-    cout << "Flow A:" << std::endl;
-    bool first = true;
-    while (!flowA.empty()) {
-      if (!first) {
-        cout << ", ";
-      }
-      first = false;
-
-      cout << flowA.top();
-      flowA.pop();
-    }
-    cout << std::endl;
+  int totalStars = 0;
+  for (auto &pair : starCount) {
+    totalStars += pair.second;
   }
 
 
-  if (flowB.empty()) {
-    cout << "Flow B:" << std::endl << "<EMPTY>" << std::endl;
-  } else {
-    cout << "Flow B:" << std::endl;
 
-    bool first = true;
-    while (!flowB.empty()) {
-      if (!first) {
-        cout << ", ";
-      }
-      first = false;
-      cout << flowB.front();
-      flowB.pop();
+  if (totalStars > 0) {
+    cout << "Stars: " << totalStars << std::endl;
+
+    for (const auto &pair : starCount) {
+      cout << STARS[pair.first] << ": " << pair.second << std::endl;
     }
-    cout << std::endl;
   }
+
+  if (planetCount > 0) {
+    cout << "Planets: " << planetCount << std::endl;
+  }
+
+  if (asteroidCount + cometCount > 0) {
+    cout << "Asteroids/comets: " << asteroidCount + cometCount << std::endl;
+  }
+
 
   return 0;
 }
