176cef7efbb40a113f0ce73ed371ab642f1c2a66
Violeta Mihovska <vili_mihovska@live.com>
2024-06-19 22:03:11 +0300

    fix added plus refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,16 @@
 #include <algorithm>
-#include <array>
 #include <cctype>
 #include <cfloat>
 #include <climits>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <iomanip>
-#include <ios>
 #include <iostream>
-#include <iterator>
-#include <stack>
-#include <queue>
-#include <locale>
 #include <ostream>
-#include <sstream>
 #include <string>
 #include <vector>
 #include <map>
+#include <unordered_set>
 
 #ifdef TESTING
 #include <gtest/gtest.h>
@@ -27,9 +20,9 @@ const char ASTEROID = '#';
 const char COMET = '$';
 const char EMPTY_SEGMENT = '.';
 const char REPLACE_SYMBOL = '+';
-const std::vector<char> STARS = {'O', 'B', 'A', 'F', 'G', 'K', 'M', 'L', 'T', 'Y'};
+const std::unordered_set<char> STARS = {'O', 'B', 'A', 'F', 'G', 'K', 'M', 'L', 'T', 'Y'};
 
-int process(std::istream &cin, std::ostream &cout) {
+std::vector<std::vector<char>> readMatrix(std::istream &cin) {
   std::vector<std::vector<char>> matrix;
   std::string line;
 
@@ -38,55 +31,75 @@ int process(std::istream &cin, std::ostream &cout) {
     matrix.push_back(row);
   }
 
-  std::string replacements;
-  std::getline(cin, replacements);
+  return matrix;
+}
 
-  std::map<char, int> starCount;
-  int planetCount = 0;
-  int cometCount = 0;
-  int asteroidCount = 0;
-
+void countElements(std::vector<std::vector<char>> &matrix, std::map<char, int> &starCount,
+                   int &planetCount, int &cometCount, int &asteroidCount) {
   for (const auto &row : matrix) {
     for (auto ch : row) {
       if (ch == ASTEROID) {
         asteroidCount++;
       } else if (ch == COMET) {
         cometCount++;
-      } else if (ch >= 1 && ch <= 9) {
-        planetCount++;
-      } else {
-        auto it = std::find(STARS.begin(), STARS.end(), ch);
-        if (it != STARS.end()) {
-          size_t pos = it - STARS.begin();
-          starCount[pos]++;
-        }
-       }
+      } else if (std::isdigit(ch)) {
+        planetCount += ch - '0';
+      } else if (STARS.count(ch)) {
+        starCount[ch]++;
+      }
     }
   }
+}
+
+int process(std::istream &cin, std::ostream &cout) {
+  std::vector<std::vector<char>> matrix = readMatrix(cin);
+
+  std::string replacements;
+  std::getline(cin, replacements);
+
+  std::map<char, int> starCount;
+
+  int planetCount = 0;
+  int cometCount = 0;
+  int asteroidCount = 0;
+
+  countElements(matrix, starCount, planetCount, cometCount, asteroidCount);
 
   int totalStars = 0;
-  for (auto &pair : starCount) {
-    totalStars += pair.second;
-  }
-
-
-
-  if (totalStars > 0) {
-    cout << "Stars: " << totalStars << std::endl;
-
-    for (const auto &pair : starCount) {
-      cout << STARS[pair.first] << ": " << pair.second << std::endl;
+  if (!starCount.empty()) {
+    for (const auto &[firstPairStar, secondPairStar] : starCount) {
+      totalStars += secondPairStar;
     }
   }
 
-  if (planetCount > 0) {
+  if (totalStars >= 0) {
+    cout << "Stars: " << totalStars << std::endl;
+
+    for (const auto &[firstPair, secondPair] : starCount) {
+      cout << "- " << firstPair << ": " << secondPair << std::endl;
+    }
+  }
+
+  if (planetCount >= 0) {
     cout << "Planets: " << planetCount << std::endl;
   }
 
-  if (asteroidCount + cometCount > 0) {
+  if (asteroidCount + cometCount >= 0) {
     cout << "Asteroids/comets: " << asteroidCount + cometCount << std::endl;
   }
 
+  std::vector<std::vector<char>> newMatrix = matrix;
+
+  for (int i = 0; i < matrix.size(); ++i) {
+    for (int j = 0; j < matrix[i].size(); ++j) {
+      char currentChar = matrix[i][j];
+      if (replacements.find(currentChar) != std::string::npos) {
+        newMatrix[i][j] = REPLACE_SYMBOL;
+      }
+      cout << newMatrix[i][j];
+    }
+    cout << std::endl;
+  }
 
   return 0;
 }
